ac6a27f16a78dde22ed151fa29bc051a906679ea
groshinio <michal.grochalski@gmail.com>
2013-03-12 23:26:41 +0100

    Dodanie klasy z mozliwoscia zapytania bazodanowego

--- class/managers/DatabaseManager.class.php
+++ class/managers/DatabaseManager.class.php
@@ -4,18 +4,50 @@ class DatabaseManager {
 	
 	static public function getConnection() {
 		
-		$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASS, DB_DB);		
+		$conn = @new mysqli(DB_SERVER, DB_USERNAME, DB_PASS, DB_DB);		
 
 		if(mysqli_connect_errno()){
 			$conn_errno = mysqli_connect_errno();
 			$conn_error = mysqli_connect_error();
+			//W przypadku wystąpenia problemów z połączeniem, zapisz informację w pliku LOG i zakończ działanie skrytpu
+			LogFile::AdLog("Wystąpił błąd połączenia numer . [".$conn_errno."]  z bazą danych o treści: [ $conn_error ]", __LINE__, __FILE__);
+			exit();
 			
-			return "Wystąpił błąd połączenia z bazą danych. [".$conn_errno."] $conn_error";
 		} else {
 			
 			$conn->query("SET NAMES 'utf8'");
-			return "Połączono z bazą danych";
+			return $conn;
 		}
+	}
+	
+	static public function selectSql($sql) {
+		
+		$conn = self::getConnection();
+		$sql = $conn->real_escape_string($sql); // chroni przed wstrzyknięciem kodu backflashem
+		$result = $conn->query($sql);
+		
+		if(!$result){
+			
+			LogFile::AddLog("Wystąpił błąd połączenia z bazą danych!", __LINE__, __FILE__);
+			return FALSE;
+		
+		} else {
+			
+			$resultArray = Array();
+			
+				while(($row = $reault->fetch_array(MYSQLI_ASSOC)) !== NULL) {
+					
+					$resultArray[] = $row;
+				}
+		}
+		if(count($resultArray) > 0 ) {
+			return $resultArray;
+		} esle {
+			LogFile::AddLog("Zapytanie bazodanowe zwróciło pusty wynik", __LINE__, __FILE__);
+			return FALSE;			
+		}
+		
+		mysqli_close($conn);
 	}
 }
 
--- index.php
+++ index.php
@@ -5,5 +5,7 @@ echo "<pre>";
 
 print_r(DatabaseManager::getConnection());
 
+print_r(DatabaseManager::selectSql("SELECT * FROM users"));
+
 echo "</pre>";
 ?>
